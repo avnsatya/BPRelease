37dbbddaba38b11ba695bce6f8ccebce8307294f
avnsatya <55024031+avnsatya@users.noreply.github.com>
2021-03-26 14:55:54 +0530

    Update objectdeploy.bat

--- release 1/objects/objectdeploy.bat
+++ release 1/objects/objectdeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Object - Test.xml" /overwrite /sso /dbconname %3
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Object - SatyaTest_GIT.xml" /overwrite /sso /dbconname %3
